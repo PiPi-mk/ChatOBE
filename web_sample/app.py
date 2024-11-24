4e1d09165d03d042fd95111186852961471dce3f
fanhaotian <1592276395@qq.com>
2024-11-24 22:47:33 +0800

    更新接口：换成最新版本的openai-api

--- web_sample/app.py
+++ web_sample/app.py
@@ -2,8 +2,13 @@ import openai
 import markdown
 from flask import Flask, render_template, request, jsonify
 
-# 设置 OpenAI API 密钥
-# openai.api_key = 'your-api-key'
+"""
+author: FHT
+这里用的是我自己的openai-api-key
+github保护机制不允许上传
+所以运行的时候要把下面这行注释取消，并换成群里的key
+"""
+# openai.api_key = "your-key"
 
 # 创建 Flask 应用
 app = Flask(__name__)
@@ -11,18 +16,19 @@ app = Flask(__name__)
 # 存储对话历史
 conversation_history = []
 
+
 # 创建函数与 OpenAI API 进行交互
 def get_ai_response(user_input):
     # 将用户消息添加到历史中
     conversation_history.append({"role": "user", "content": user_input})
 
     # 与 OpenAI API 交互，传递整个对话历史
-    response = openai.ChatCompletion.create(
+    response = openai.chat.completions.create(
         model="gpt-4o", messages=conversation_history  # 使用 gpt-3.5-turbo 模型
     )
 
     # 获取 AI 回复并将其添加到历史中
-    ai_message = response["choices"][0]["message"]["content"].strip()
+    ai_message = response.choices[0].message.content.strip()
     ai_message = ai_message.replace("| |", "|\n|").replace("\\n", "\n")
     conversation_history.append({"role": "assistant", "content": ai_message})
 
@@ -31,11 +37,13 @@ def get_ai_response(user_input):
 
     return md_message
 
+
 # 首页
 @app.route("/")
 def index():
     WELCOME_MSG = "欢迎使用chatOBE~ \n 我是结合了大语言模型的OBE系统，可以帮你进行选课、查询等等。需要我帮你做些什么？"
     return render_template("index.html", initial_message=WELCOME_MSG)
+
 
 # 处理用户消息
 @app.route("/ask", methods=["POST"])
@@ -43,6 +51,7 @@ def ask():
     user_message = request.form["message"]
     ai_message = get_ai_response(user_message)
     return jsonify({"ai_message": ai_message})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
