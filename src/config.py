d58bfb9dba78495cd50c5dab6b7feef2e4b6c3ea
fanhaotian <1592276395@qq.com>
2024-11-26 09:59:00 +0800

    整理配置文件

---
+++ src/config.py
@@ -0,0 +1,17 @@
+DATABASE_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "charles@mysql0618",
+    "port": 3306,
+    "charset": "utf8mb4",
+    "database": "sc"
+}
+
+
+"""
+author: FHT
+这里用的是我自己的openai-api-key
+github保护机制不允许上传
+所以运行的时候要把下面这行注释取消，并换成群里的key
+"""
+OPENAI_API_KEY = "your-key"
